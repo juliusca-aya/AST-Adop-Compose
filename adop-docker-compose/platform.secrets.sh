7a77478a64ec81c3a5b9fce3df70cc2cb243090f
EC2 Default User <ec2-user@ip-172-31-32-43.eu-west-1.compute.internal>
2016-04-25 06:24:17 +0000

    AST Backup

---
+++ adop-docker-compose/platform.secrets.sh
@@ -0,0 +1,16 @@
+# Example file to store all the generated secrets for your ADOP stack
+
+# Username for initial admin user
+export INITIAL_ADMIN_USER="pdcdevops"
+
+# Password for initial admin user
+export INITIAL_ADMIN_PASSWORD_PLAIN="Accenture@0123"
+
+# Password for the Jenkins user
+export PASSWORD_JENKINS="e082b90821846191"
+
+# Password for the Gerrit user
+export PASSWORD_GERRIT="0e9a6ebfd1a21181"
+
+# Root password for the Sonar and Gerrit MySQL instances
+export PASSWORD_SQL="a7e2531c53cc2094"
